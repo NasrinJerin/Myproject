fbf1347f92f5d1e6fb3bff72acd312ee073627c1
NasrinJerin <nasrinjerin007@gmail.com>
2024-06-17 22:11:23 +0600

    Update git.cpp

--- git.cpp
+++ git.cpp
@@ -3,6 +3,10 @@ void myFunction() {
   cout << "I just got executed!";
 }
 
+void myFunction2() {
+  cout << "I just got executed!";
+}
+
 int main() {
   myFunction(); // call the function
   return 0;
